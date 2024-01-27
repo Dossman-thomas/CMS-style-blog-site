4126a7a5661c4ddb1d6ad6e15183761138394d50
Tom Dossman <tdossman.development@gmail.com>
2024-01-27 18:53:34 -0500

    userRoutes initialized and user-related activities established

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// route to get all users 
+router.get('/', async (req, res) => {
+
+});
+
+// Sign up route (create a new user)
+router.post('/signup', async (req, res) => {
+
+});
+
+// Log in route (get a user)
+router.post('/login', async (req, res) => {
+
+});
+
+// Log out route
+router.post('/logout', async (req, res) => {
+
+});
+
+
+
+
+
