04aff7fbf2edf754025073d74fbd54aa45605f54
Tom Dossman <tdossman.development@gmail.com>
2024-01-27 10:29:05 -0500

    models initialized

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User'); // import User model
+const Post = require('./Post'); // import Post model
+const Comment = require('./Comment'); // import Comment model
+
+
+
+
+
+module.exports = { User, Post, Comment }; // Export models and relationships
