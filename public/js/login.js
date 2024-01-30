14c1be522f7ef6fe5695c49d4ad99c3058645251
Tom Dossman <tdossman.development@gmail.com>
2024-01-30 12:12:37 -0500

    login, signup, and logout functionality configured

--- public/js/login.js
+++ public/js/login.js
@@ -1,53 +1,35 @@
+// Login Form Handler Asynchronous Function
 const loginFormHandler = async (event) => {
-  event.preventDefault();
 
-  // Collect values from the login form
+  event.preventDefault(); // prevents the default behavior of form submission, allowing the function to handle the submission manually
+
+  // retrieve the values entered in the email and password fields of the login form and trim any leading or trailing whitespace
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
+  if (email && password) { // ensures that both email and password have non-empty values
+
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      // If successful, redirect the browser to the home page
+      document.location.replace('/');
     } else {
-      alert(response.statusText);
+      alert(response.statusText); // If the response is not successful, an alert is shown with the status text
     }
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
 
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
+// Attaches event listener to the submit events of the login form, triggering the form handler function when the forms are submitted
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
 
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+
---
+++ public/js/signup.js
@@ -0,0 +1,33 @@
+// Signup Form Handler Asynchronous Function
+const signupFormHandler = async (event) => {
+
+  event.preventDefault(); // prevents the default form submission behavior
+
+  // retrieve the values entered in the username, email, and password fields of the login form and trim any leading or trailing whitespace
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) { // Check if name, email, and password are provided
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+
+      document.location.replace('/'); // If successful, redirect the browser to the home page
+
+    } else {
+
+      alert(response.statusText); // If the response is not successful, an alert is shown with the status text
+
+    }
+  }
+};
+
+// Attaches event listener to the submit events of the signup form, triggering the form handler function when the forms are submitted
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
