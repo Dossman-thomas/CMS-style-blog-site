6f7a425d1506b529ff899ac0ca245f319fa7a7d3
Tom Dossman <tdossman.development@gmail.com>
2024-02-07 12:34:24 -0500

    join.js deleted

--- public/js/join.js
+++
@@ -1,7 +0,0 @@
-const joinNowButton = document.querySelector('#joinNowButton');
-
-// Add an event listener to the button
-joinNowButton.addEventListener('click', function() {
-  // Redirect the user to the '/signup' page
-  document.location.replace('/signup');
-});
